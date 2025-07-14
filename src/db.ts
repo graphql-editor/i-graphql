b7b72327d94fc87f06a29616c49ce9e3caf4e20d
aexol <aexol@aexol.com>
2025-07-14 19:50:05 +0200

    url inject ;p

--- src/db.ts
+++ src/db.ts
@@ -1,12 +1,16 @@
 import { Db, MongoClient } from 'mongodb';
 let mongoConnection: { db: Db; client: MongoClient } | undefined = undefined;
 
-export const mc = async (props: { afterConnection?: (database: Db) => void; mongoClient?: MongoClient }) => {
-  const { afterConnection, mongoClient } = props;
+export const mc = async (props: {
+  afterConnection?: (database: Db) => void;
+  mongoClient?: MongoClient;
+  mongoUrl?: string;
+}) => {
+  const { afterConnection, mongoClient, mongoUrl } = props;
   if (mongoConnection) {
     return Promise.resolve(mongoConnection);
   }
-  const resolvedMongoUrl = process.env.MONGO_URL;
+  const resolvedMongoUrl = mongoUrl || process.env.MONGO_URL;
   let client: MongoClient | undefined = mongoClient;
   if (resolvedMongoUrl && !client) {
     client = new MongoClient(resolvedMongoUrl, {
@@ -14,13 +18,13 @@ export const mc = async (props: { afterConnection?: (database: Db) => void; mong
     });
   }
   if (!client) {
-    throw new Error('Please provide MONGO_URL environment variable');
+    throw new Error('Please provide MONGO_URL environment variable or pass mongoUrl as a prop');
   }
   const c = await client.connect();
   const db = c.db();
   const dbName = db.databaseName;
   if (!dbName || dbName === 'test') {
-    throw new Error("Provide database name inside MONGO_URL to work with iGraphQL. 'test' is also not accepted");
+    throw new Error("Provide database name inside mongo url to work with iGraphQL. 'test' is also not accepted");
   }
   mongoConnection = {
     client,
